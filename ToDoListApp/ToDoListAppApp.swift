7482b5584da0801462a49100002a8f3aec1fd942
Jacob Colon <jj.colon7802@gmail.com>
2025-01-22 15:27:17 -0500

    Add files via upload

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Jacob Colon on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
